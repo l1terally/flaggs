b0f97ac57ce82edac4b083b4288b9f87eb9b9a7f
l1terally <literallyuriy@gmail.com>
2023-01-05 20:23:39 +0800

    Initial Commit

---
+++ flaggs/flaggsApp.swift
@@ -0,0 +1,17 @@
+//
+//  flaggsApp.swift
+//  flaggs
+//
+//  Created by Юрий  Лизунов on 05/01/23.
+//
+
+import SwiftUI
+
+@main
+struct flaggsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
