bc3b2ea79a5abd52e338a8b34c9144e14207c8a4
l1terally <literallyuriy@gmail.com>
2023-01-05 20:25:31 +0800

    Game created

--- flaggs/ContentView.swift
+++ flaggs/ContentView.swift
@@ -1,26 +1,107 @@
 //
 //  ContentView.swift
-//  flaggs
+//  flags
 //
-//  Created by Юрий  Лизунов on 05/01/23.
+//  Created by Юрий  Лизунов on 13/11/22.
 //
 
 import SwiftUI
 
 struct ContentView: View {
+    
+    
+    @State private var countries = ["UK", "USA", "Bangladesh", "Germany", "Argentina", "Brazil", "Canada", "Greece", "Russia", "Sweden"].shuffled()
+    @State private var correctAnswer = Int.random(in: 0...2)
+    @State private var score = 0
+    @State private var showingScore = false
+    @State private var scoreTitle = ""
+    
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack{
+            LinearGradient(gradient: Gradient(colors: [.black, .white]), startPoint: .top, endPoint: .bottom).edgesIgnoringSafeArea(.all)
+            VStack(spacing: 30){
+                
+                
+                VStack{
+                    Text("Choose the flag")
+                        .foregroundColor(.white)
+                        .font(.headline)
+                    Text(countries[correctAnswer])
+                        .foregroundColor(.white)
+                        .font(.largeTitle)
+                        .fontWeight(.black)
+                }
+                ForEach(0..<3) { number in
+                    Button(action: {
+                        self.flagTapped(number)
+                        self.showingScore = true
+                    }) {
+                        Image(self.countries[number])
+                            .renderingMode(.original)
+                            .frame(width: 250, height: 125)
+                            .clipShape(Capsule())
+                            .overlay(Capsule().stroke(Color.black, lineWidth: 1))
+                            .shadow(color: .black, radius: 2)
+                    }
+                }
+                Text("Overall score \(score)")
+                    .font(.largeTitle)
+                    .fontWeight(.black)
+                
+                
+                Spacer()
+            }
+        } .alert(isPresented: $showingScore) {
+            Alert(title: Text(scoreTitle), message: Text("Overall score: \(score)"), dismissButton: .default(Text("Continue")) {
+                self.askQuestion()
+            })
         }
-        .padding()
+    }
+        func askQuestion() {
+            countries.shuffle()
+            correctAnswer = Int.random(in: 0...2)
+    }
+    func flagTapped(_ number: Int) {
+        if number == correctAnswer {
+            scoreTitle = "Correct!"
+            score += 1
+        } else {
+            scoreTitle = "Oops..., that's \(countries[number])"
+            score -= 1
+        }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+
